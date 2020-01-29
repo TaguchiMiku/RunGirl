be32a1938167b42fe5c6f70c948114bcafb8e0c6
TaguchiMiku <test@test.tes>
2020-01-29 11:30:44 +0900

    wasure

---
+++ Resources/image/Environment/tilesets_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tilesets_2" tilewidth="25" tileheight="25" tilecount="23280" columns="194">
+ <image source="tilesets_2.png" width="4866" height="3000"/>
+</tileset>
